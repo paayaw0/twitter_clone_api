f948e07683867112ad225bfb5944eb1c5bf88d9d
Paa Yaw <paayaw.dev@gmail.com>
2023-07-15 09:37:31 +0000

    declare self-join to allow twweets to have retweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,6 +3,9 @@ class Tweet < ApplicationRecord
 
   has_one_attached :media
 
+  has_many :retweets, dependent: :destroy, class_name: 'Tweet', foreign_key: 'tweet_id'
+  belongs_to :tweet, optional: true
+
   validates_with TweetValidator
   validates :media, allow_blank: true, media_support: true, media: true
   validates :content, allow_blank: true, length: { maximum: 140, message: 'character limit of 140 exceeded!' }
